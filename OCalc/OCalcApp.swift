31afd944a188433caf0fc7757569ec3cf521eb96
Kaneis Zontanos <ramim@bc.edu>
2023-10-31 22:26:59 -0400

    Initial Commit

---
+++ OCalc/OCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  OCalcApp.swift
+//  OCalc
+//
+//  Created by Kaneis Zontanos on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct OCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
